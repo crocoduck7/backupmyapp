be2ee5c251e2e4e07de34a32f4bd1e78c7aed7ae
Tabunov Mikhail <tmp.over@gmail.com>
2009-06-24 12:57:42 +0400

    Added capistrano tasks

---
+++ recipes/backupmyapp.cap.rb
@@ -0,0 +1,19 @@
+namespace :backupmyapp do
+  desc "Test connection to backupmyapp.com"
+  task :test, :only => { :primary => true } do
+    rails_env = fetch(:rails_env, "production")
+    run "cd #{current_path}; rake backupmyapp:test RAILS_ENV=#{rails_env}"
+  end
+
+  desc "Send backup to backupmyapp.com"
+  task :backup, :only => {:primary => true } do
+    rails_env = fetch(:rails_env, "production")
+    run "cd #{current_path}; rake backupmyapp:backup RAILS_ENV=#{rails_env}"
+  end
+
+  desc "Restore backup from backupmyapp.com"  
+  task :restore, :only => {:primary => true } do
+    rails_env = fetch(:rails_env, "production")
+    run "cd #{current_path}; rake backupmyapp:restore RAILS_ENV=#{rails_env}"
+  end
+end
