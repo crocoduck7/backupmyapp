7995d3fc9b72b60583fb55ac7ab8b1e489fb2e39
Tabunov Mikhail <tmp.over@gmail.com>
2009-08-03 19:08:12 +0400

    Fixed add route

--- init.rb
+++ init.rb
@@ -1,10 +1,10 @@
 ActionController::Base.send(:include, BackupmyappInit)
 
 ActionController::Routing::RouteSet.class_eval do
-  def load_routes_with_named_routes!
-    load_routes_without_named_routes!
-    BackupmyappInit.add_routes 
+  def clear_with_clear_backupmyapp!
+    clear_without_clear_backupmyapp!
+    add_route 'backupmyapp', :controller => 'application', :action => 'backupmyapp'
   end
- 
-  alias_method_chain :load_routes!, :named_routes
+  
+  alias_method_chain :clear!, :clear_backupmyapp
 end
