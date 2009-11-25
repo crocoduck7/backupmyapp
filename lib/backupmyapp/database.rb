1102ac6805d105b471612f2854b0e0bf8c287ba7
Tabunov Mikhail <tmp.over@gmail.com>
2009-11-25 17:57:49 +0300

    Now dump database to /tmp folder

--- lib/backupmyapp/database.rb
+++ lib/backupmyapp/database.rb
@@ -6,8 +6,8 @@ class Backupmyapp
 
     def self.backup
       timestamp = Time.now.utc.strftime("%Y%m%d%H%M%S")
-      dump_dir = "#{RAILS_ROOT}/db/backupmyapp/#{timestamp}"
-      FileUtils.rm_r("#{RAILS_ROOT}/db/backupmyapp/") if File.exists?("#{RAILS_ROOT}/db/backupmyapp/")
+      dump_dir = "#{RAILS_ROOT}/tmp/backupmyapp/#{timestamp}"
+      FileUtils.rm_r("#{RAILS_ROOT}/tmp/backupmyapp/") if File.exists?("#{RAILS_ROOT}/tmp/backupmyapp/")
       FileUtils.mkdir_p(dump_dir)
       MarshalDb.dump(dump_dir)
     end
