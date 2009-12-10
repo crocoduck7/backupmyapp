19024882e7b7d2b0905b5cf9a198afe1ce800348
Tabunov Mikhail <tmp.over@gmail.com>
2009-12-10 19:25:16 +0300

    Fix issue with route generation

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 ActionController::Base.send(:include, Backupmyapp::Init)
 
-if Rails.version <= '2.3.0'
+if Rails.version < '2.3.0'
   ActionController::Routing::RouteSet.class_eval do
     def clear_with_clear_backupmyapp!
       clear_without_clear_backupmyapp!
