add691f9509811f193ec7bbaf0f80d9f0b85f0df
Tabunov Mikhail <tmp.over@gmail.com>
2010-03-06 15:16:56 +0300

    Upload collection in threads

--- lib/backupmyapp/network.rb
+++ lib/backupmyapp/network.rb
@@ -67,7 +67,16 @@ class Backupmyapp
     end
     
     def upload_collection(collection, try = 0)
-      collection.each {|file| upload(file) }
+      collection.each_slice(6) do |slice|
+        threads = []
+        slice.each do |file| 
+          threads << Thread.new do
+            upload(file)
+          end
+        end
+        
+        threads.each {|t| t.join }
+      end
       retry_failed_transfers("upload", try)
     end
   
